d501199056992ee6fed26c0f51fb18a6794ef54e
JongHyun Lee <112399028+JongHyunLee84@users.noreply.github.com>
2023-01-06 18:07:03 +0900

    Add files via upload

---
+++ Memorizing/Extension/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  ColorExtension.swift
+//  Memorizing
+//
+//  Created by 진준호 on 2023/01/05.
+//
+
+import SwiftUI
+
+extension Color{
+    static let mainDarkBlue = Color("MainDarkBlue")
+    static let mainBlue = Color("MainBlue")
+    static let mainBlack = Color("MainBlack")
+    static let gray1 = Color("Gray1")
+    static let gray2 = Color("Gray2")
+    static let gray3 = Color("Gray3")
+    static let gray4 = Color("Gray4")
+    static let gray5 = Color("Gray5")
+    static let gray6 = Color("Gray6")
+    static let gray7 = Color("Gray7")
+    static let gray8 = Color("Gray8")
+    static let EnglishColor = Color("EnglishColor")
+    static let HistoryColor = Color("HistoryColor")
+    static let ITColor = Color("ITColor")
+    static let EconomyColor = Color("EconomyColor")
+    static let KnowledgeColor = Color("KnowledgeColor")
+    static let EtcColor = Color("EtcColor")
+}
